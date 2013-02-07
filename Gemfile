4fa0fab15db203c25974e1d140c78ecf83eb6bb1
Daria Grishchenko <d.grishchenko@111minutes.com>
2013-02-07 13:00:11 -0800

    Project setup

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'mysql2'
+gem 'jquery-rails'
+gem 'nokogiri'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test, :development do
+  gem 'rspec-rails', '~> 2.8.0'
+  gem 'thin'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmailLists::Application.config.secret_token = '90082790b28014de1b747b289cfeb3abc845fa2848dffe59b9d6eb5cd991b518f615b879db5590dfc1037ef5156c61f83de7b72e039216957692786d180f8246'
