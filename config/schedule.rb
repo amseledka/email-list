1999db56242e32e40a3b9842ed6b74cde36b7cc6
Daria Grishchenko <d.grishchenko@111minutes.com>
2013-02-14 14:16:46 -0800

    test whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,8 +2,8 @@ set :output, "log/cron_log.log"
 set :environment, 'development' 
 job_type :runner, "cd :path && bundle exec script/rails runner -e :environment ':task' :output"
 
-every :day, :at => '11:00pm' do
+every :day, :at => '02:30am' do
   runner "Subscriber.export_emails_to_csv"
-  rake "spec"
+  rake "spec"`
   runner "Subscriber.update_added_emails"
 end
