9ddfe604df3920cf03c3df57338eeff509c7bdd0
Daria Grishchenko <d.grishchenko@111minutes.com>
2013-02-19 22:34:52 -0800

    Cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,8 +2,10 @@ set :output, "log/cron_log.log"
 set :environment, 'development'
 set :job_template, "bash -l -i -c ':job'"
 job_type :runner, 'source /opt/rvm/environments/ruby-1.9.3-p194@email_lists && bundle install && cd :path && bundle exec script/rails runner -e :environment ":task" :output'
+job_type :rake, 'source /opt/rvm/environments/ruby-1.9.3-p194@email_lists && bundle install && cd :path && RAILS_ENV=:environment bundle exec rake :task --silent :output'
 
-every :day, :at => '10:20am' do
+
+every :day, :at => '10:40am' do
   runner 'Subscriber.export_emails_to_csv'
   rake "spec"
   runner "Subscriber.update_added_emails"
