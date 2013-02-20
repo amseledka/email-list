9d2ad5cf646b9b0bc862f698003fbe97a804ab6a
Daria Grishchenko <d.grishchenko@111minutes.com>
2013-02-19 21:47:20 -0800

    Add more fix for whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,9 @@
 set :output, "log/cron_log.log"
-set :environment, 'development' 
+set :environment, 'development'
+set :job_template, "bash -l -i -c ':job'"
 job_type :runner, 'cd :path && bundle exec script/rails runner -e :environment ":task" :output'
 
-every :day, :at => '9:43am' do
+every :day, :at => '9:50am' do
   runner 'Subscriber.export_emails_to_csv'
   rake "spec"
   runner "Subscriber.update_added_emails"
