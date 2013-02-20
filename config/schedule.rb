d7500b2728253c7e6984141a9b15ff1c1bef2679
Daria Grishchenko <d.grishchenko@111minutes.com>
2013-02-19 21:16:50 -0800

    Remove backslashes from runner

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 set :output, "log/cron_log.log"
 set :environment, 'development' 
-job_type :runner, "cd :path && bundle exec script/rails runner -e :environment ':task' :output"
+job_type :runner, 'cd :path && bundle exec script/rails runner -e :environment ":task" :output'
 
-every :day, :at => '9:05am' do
-  runner "Subscriber.export_emails_to_csv"
+every :day, :at => '9:20am' do
+  runner 'Subscriber.export_emails_to_csv'
   rake "spec"
   runner "Subscriber.update_added_emails"
 end
