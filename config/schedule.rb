eaef0cc2932ec01b9642b5ec9e4aed114de38834
Daria Grishchenko <d.grishchenko@111minutes.com>
2013-02-19 22:14:48 -0800

    More cron fixes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 set :output, "log/cron_log.log"
 set :environment, 'development'
 set :job_template, "bash -l -i -c ':job'"
-job_type :runner, 'cd :path && bundle exec script/rails runner -e :environment ":task" :output'
+job_type :runner, 'source /opt/rvm/environments/ruby-1.9.3-p194@email_lists && bundle install && cd :path && bundle exec script/rails runner -e :environment ":task" :output'
 
-every :day, :at => '9:50am' do
+every :day, :at => '10:20am' do
   runner 'Subscriber.export_emails_to_csv'
   rake "spec"
   runner "Subscriber.update_added_emails"
